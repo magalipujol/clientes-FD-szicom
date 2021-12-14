398e1fdcfc605b6634f0b301fa2a383c98406c16
Magali Pujol <74333307+magalipujol@users.noreply.github.com>
2021-12-14 14:11:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clientes_DC_szicom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
