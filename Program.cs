fe090e64e33f57bef6ea0d6ae385687bdd3e8d97
magali <magalipuuujol@gmail.com>
2021-12-15 14:54:43 -0300

    metodo para encontrar laCDC

--- Program.cs
+++ Program.cs
@@ -13,14 +13,15 @@ namespace clientes_DC_szicom
             var transportes = Parser.TxtToTteTerceros("C:/Users/magalip/Documents/Datos problema/Datos Problemas/tte_terceros-ago-2017.txt");
             DateTime fechaInicio = new DateTime(2017, 8, 1);
             DateTime fechaFin = new DateTime(2017, 8, 31); 
-            var query = transportes.Where(t=> t.DiaOperativo == fechaInicio);
-            foreach (var item in query)
-            {
+            var servicios = Parser.TxtToVolServicio("C:/Users/magalip/Documents/Datos problema/Datos Problemas/volumen_servicio_v2.txt");
 
-                Console.WriteLine(item.IdCliente);
-            }
+            int unaSuma = servicios.Where(s => s.FechaInicio <= fechaInicio && s.FechaFin >= fechaFin && s.IdCliente == 665)
+                          .Sum(s => s.CDC);
+            Console.WriteLine(unaSuma);
+             
 
-            
+
+
 
         }
     }
